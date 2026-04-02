26a1bce84580cb45f2304352785744572aa794ae
KSOCoder <calvin@Calvin-Laptop.localdomain>
2026-04-02 07:11:59 -0400

    Added Win message for respective player

--- TicTacToe.asm
+++ TicTacToe.asm
@@ -3,6 +3,9 @@ board     db '1','2','3','4','5','6','7','8','9', 10
 turn      db 'X'
 newline   db 10
 
+xWinMsg   db "X wins!", 10
+oWinMsg   db "O wins!", 10
+
 xQueue    db 0FFh,0FFh,0FFh
 oQueue    db 0FFh,0FFh,0FFh
 xHead     db 0
@@ -10,9 +13,10 @@ oHead     db 0
 xCount    db 0
 oCount    db 0
 
-rows      db 0,1,2, 3,4,5, 6,7,8
-cols      db 0,3,6, 1,4,7, 2,5,8
-diags     db 0,4,8, 2,4,6
+lines      db 0,1,2, 3,4,5, 6,7,8, 0,3,6, 1,4,7, 2,5,8, 0,4,8, 2,4,6
+
+section .bss
+input resb 2
 
 section .text
 global _start
@@ -42,10 +46,14 @@ gameLoop:
 
  .winner:
    call printBoard
+   mov al, [turn]
+   cmp al, 'X'
+   lea ecx, [xWinMsg]
+   mov edx, 7
+   lea ecx, [oWinMsg]
+   mov edx, 7
    mov eax, 4
    mov ebx, 1
-   mov ecx, board
-   mov edx, 10
    int 0x80
    ret
 
