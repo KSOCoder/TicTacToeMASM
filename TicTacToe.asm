34c7138c36ff5aeb97eacbf321e24dfe81d30109
KSOCoder <calvin@Calvin-Laptop.localdomain>
2026-04-02 05:17:02 -0400

    Initial commit of TicTacToe

---
+++ TicTacToe.asm
@@ -0,0 +1,19 @@
+section .data
+board     db '1','2','3','4','5','6','7','8','9', 10
+turn      db 'X'
+
+xQueue    db 0FFh,0FFh,0FFh
+oQueue    db 0FFh,0FFh,0FFh
+xHead     db 0
+oHead     db 0
+xCount    db 0
+oCount    db 0
+
+rows      db 0,1,2, 3,4,5, 6,7,8
+cols      db 0,3,6, 1,4,7, 2,5,8
+diags     db 0,4,8, 2,4,6
+
+section .text
+global _start
+_start:
+  syscall
