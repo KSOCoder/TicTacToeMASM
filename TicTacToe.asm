347dc232bfa2f1b5160b34ae3b5f56945005644a
KSOCoder <calvin@Calvin-Laptop.localdomain>
2026-04-02 05:43:45 -0400

    Main flow

--- TicTacToe.asm
+++ TicTacToe.asm
@@ -1,6 +1,7 @@
 section .data
 board     db '1','2','3','4','5','6','7','8','9', 10
 turn      db 'X'
+newline   db 10
 
 xQueue    db 0FFh,0FFh,0FFh
 oQueue    db 0FFh,0FFh,0FFh
@@ -16,4 +17,25 @@ diags     db 0,4,8, 2,4,6
 section .text
 global _start
 _start:
-  syscall
+  call gameLoop
+
+  mov eax, 1
+  xor ebx, ebx
+  int 0x80
+
+printNewLine:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, newline
+  mov edx, 1
+  int 0x80
+  ret
+
+gameLoop:
+ .loopStart:
+   ;call printBoard
+   ;call getInput
+   ;call placeMark
+   ;call checkWin
+   ;jc .winner
+   ;jmp .loopStart
